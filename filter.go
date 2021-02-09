e9434ebf2436dc0e9868a1de21775dd28f9d6470
Andreas Pape <andreas.pape@euroleasing.eu>
2021-02-09 10:12:19 +0100

    remove slashes from regex-search

--- filter.go
+++ filter.go
@@ -33,25 +33,25 @@ func interpretCondition(condition golik.Condition) (bson.M, error) {
 func interpretOperand(op golik.Operand) (bson.M, error) {
 	switch op.Operator() {
 	case golik.EQ:
-		return bson.M{op.Attribute() : bson.M{"$eq" : op.Value() }}, nil
+		return bson.M{op.Attribute(): bson.M{"$eq": op.Value()}}, nil
 	case golik.NE:
-		return bson.M{op.Attribute() : bson.M{"$ne" : op.Value() }}, nil
+		return bson.M{op.Attribute(): bson.M{"$ne": op.Value()}}, nil
 	case golik.CO:
-		return bson.M{op.Attribute() : bson.M{"$regex" : fmt.Sprintf("/^.*%v.*$/", op.Value()) }}, nil
+		return bson.M{op.Attribute(): bson.M{"$regex": fmt.Sprintf("^.*%v.*$", op.Value())}}, nil
 	case golik.SW:
-		return bson.M{op.Attribute() : bson.M{"$regex" : fmt.Sprintf("/^%v/", op.Value()) }}, nil
+		return bson.M{op.Attribute(): bson.M{"$regex": fmt.Sprintf("^%v", op.Value())}}, nil
 	case golik.EW:
-		return bson.M{op.Attribute() : bson.M{"$regex" : fmt.Sprintf("/%v$/", op.Value()) }}, nil
+		return bson.M{op.Attribute(): bson.M{"$regex": fmt.Sprintf("%v$", op.Value())}}, nil
 	case golik.PR:
-		return bson.M{op.Attribute() : bson.M{"$exists" : true }}, nil
+		return bson.M{op.Attribute(): bson.M{"$exists": true}}, nil
 	case golik.GT:
-		return bson.M{op.Attribute() : bson.M{"$gt" : op.Value() }}, nil
+		return bson.M{op.Attribute(): bson.M{"$gt": op.Value()}}, nil
 	case golik.GE:
-		return bson.M{op.Attribute() : bson.M{"$gte" : op.Value() }}, nil
+		return bson.M{op.Attribute(): bson.M{"$gte": op.Value()}}, nil
 	case golik.LT:
-		return bson.M{op.Attribute() : bson.M{"$lt" : op.Value() }}, nil
+		return bson.M{op.Attribute(): bson.M{"$lt": op.Value()}}, nil
 	case golik.LE:
-		return bson.M{op.Attribute() : bson.M{"$lte" : op.Value() }}, nil
+		return bson.M{op.Attribute(): bson.M{"$lte": op.Value()}}, nil
 	default:
 		return nil, fmt.Errorf("Unsupported operator %v", op.Operator())
 	}
@@ -68,7 +68,7 @@ func interpretLogical(logic golik.Logic) (bson.M, error) {
 		if err != nil {
 			return nil, err
 		}
-		return bson.M{"$and" : bson.A{l, r} }, nil
+		return bson.M{"$and": bson.A{l, r}}, nil
 	case golik.OR:
 		l, err := interpretCondition(logic.Left())
 		if err != nil {
@@ -78,7 +78,7 @@ func interpretLogical(logic golik.Logic) (bson.M, error) {
 		if err != nil {
 			return nil, err
 		}
-		return bson.M{"$or" : bson.A{l, r} }, nil
+		return bson.M{"$or": bson.A{l, r}}, nil
 	default:
 		return nil, fmt.Errorf("Unsupported logical operator %v", logic.Logical())
 	}
@@ -91,7 +91,7 @@ func interpretNot(not golik.LogicNot) (bson.M, error) {
 	}
 	result := bson.M{}
 	for key := range inner {
-		result[key] = bson.M{"$not" : inner[key] }
+		result[key] = bson.M{"$not": inner[key]}
 	}
 	return result, nil
 }
